aafd144f4056297574e366e0e75edec05607bf1c
teyeong <taetae0202@naver.com>
2023-10-30 21:00:38 +0900

    🩹 : 로고 클릭 시 메인페이지로 이동

--- src/components/_common/Header.tsx
+++ src/components/_common/Header.tsx
@@ -21,9 +21,9 @@ const Header = () => {
     // 모달 추가 예정
   };
 
-  // 로고 클릭 시 새로고침
+  // 로고 클릭 시 메인페이지로 이동
   const handleLogoClick = () => {
-    window.location.reload();
+    navigate('/');
   };
 
   return (
