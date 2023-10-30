73d3a258936c93ca7a7ca88e83ee44a0908e32c0
teyeong <taetae0202@naver.com>
2023-10-30 21:03:06 +0900

    ✨ : 광고 무한 캐러셀 구현

--- src/components/mainpage/AdList.tsx
+++ src/components/mainpage/AdList.tsx
@@ -1,5 +1,209 @@
+import { useState, useEffect, useRef, useMemo, useLayoutEffect } from 'react';
+import styled from 'styled-components';
+import data from './ad-dummy-data.json';
+import StudyDetail from './StudyDetail';
+
 const AdList = () => {
-  return <div>광고</div>;
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [current, setCurrent] = useState(1); // 현재 위치 상태
+  const [translateX, setTranslateX] = useState(0); // X축 이동으로 위치
+  const [itemCnt, setItemCnt] = useState(4); // 아이템 개수
+
+  // 화면 너비에 따른 아이템 개수 설정 useEffect
+  useEffect(() => {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth <= 660) {
+      setItemCnt(1);
+    } else if (windowWidth <= 990) {
+      setItemCnt(2);
+    } else if (windowWidth <= 1320) {
+      setItemCnt(3);
+    } else {
+      setItemCnt(4);
+    }
+  }, [window.innerWidth]);
+
+  // 데이터 설정 useMemo
+  const slides = useMemo(() => {
+    let items;
+    if (data.length > 3) {
+      items = data.map((study, index) => (
+        <StudyDetail key={index} study={study} />
+      ));
+      return [
+        <StudyDetail key={data.length + 1} study={data[data.length - 1]} />,
+        ...items,
+        <StudyDetail key={data.length + 2} study={data[0]} />,
+        <StudyDetail key={data.length + 3} study={data[1]} />,
+        <StudyDetail key={data.length + 4} study={data[2]} />,
+        <StudyDetail key={data.length + 5} study={data[3]} />,
+      ];
+    } else if (data.length <= 3 && data.length > 1) {
+      items = data.map((study, index) => (
+        <StudyDetail key={index} study={study} />
+      ));
+      setCurrent(0);
+      return [...items];
+    }
+    return <StudyDetail study={data[0]} />;
+  }, [data]);
+
+  // 레이아웃
+  useLayoutEffect(() => {
+    if (containerRef.current) {
+      setTranslateX((containerRef.current.clientWidth * current) / itemCnt);
+    }
+  }, [itemCnt]);
+
+  // 버튼 클릭 핸들러
+  const actionHandler = (mode: string) => {
+    console.log(itemCnt);
+    if (data.length <= 3) {
+      // 길이가 3보다 작거나 같은 경우 버튼 클릭 금지
+      return;
+    }
+    if (containerRef.current) {
+      containerRef.current.style.transitionDuration = '1s';
+      // 왼쪽 버튼 클릭
+      if (mode === 'prev') {
+        if (current <= 1) {
+          setTranslateX(0);
+          setCurrent(data.length);
+        } else {
+          setTranslateX(
+            (containerRef.current.clientWidth * (current - 1)) / itemCnt,
+          );
+          setCurrent((prev) => --prev);
+        }
+      }
+      // 오른쪽 버튼 클릭
+      else if (mode === 'next') {
+        if (current >= data.length) {
+          setTranslateX(
+            (containerRef.current.clientWidth * (data.length + 1)) / itemCnt,
+          );
+          setCurrent(1);
+        } else {
+          setTranslateX(
+            (containerRef.current.clientWidth * (current + 1)) / itemCnt,
+          );
+          setCurrent((prev) => ++prev);
+        }
+      }
+    }
+  };
+
+  // 무한 캐러셀 효과
+  useEffect(() => {
+    const transitionEnd = () => {
+      if (containerRef.current) {
+        if (current <= 1) {
+          containerRef.current.style.transitionDuration = '0ms';
+          setTranslateX((containerRef.current.clientWidth * current) / itemCnt);
+        }
+
+        if (current >= data.length) {
+          containerRef.current.style.transitionDuration = '0ms';
+          setTranslateX(
+            (containerRef.current.clientWidth * data.length) / itemCnt,
+          );
+        }
+      }
+    };
+
+    document.addEventListener('transitionend', transitionEnd);
+
+    return () => {
+      document.removeEventListener('transitionend', transitionEnd);
+    };
+  }, [current]);
+
+  return (
+    <Div>
+      <BtnDiv>
+        <LeftBtn onClick={() => actionHandler('prev')} />
+        <RightBtn onClick={() => actionHandler('next')} />
+      </BtnDiv>
+      <ItemDiv>
+        <div
+          ref={containerRef}
+          style={{
+            transform: `translateX(${-translateX}px)`,
+          }}
+        >
+          {slides}
+        </div>
+      </ItemDiv>
+    </Div>
+  );
 };
+
+const Div = styled.div`
+  width: 100vw;
+  height: 250px;
+  background-color: #ffe2e2;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BtnDiv = styled.div`
+  width: 1320px;
+  display: flex;
+  margin: auto;
+  margin-bottom: 0;
+  @media (max-width: 1320px) {
+    width: 990px;
+  }
+  @media (max-width: 990px) {
+    width: 660px;
+  }
+  @media (max-width: 660px) {
+    width: 330px;
+  }
+`;
+
+const LeftBtn = styled.div`
+  width: 0;
+  height: 0;
+  border-bottom: 15px solid transparent;
+  border-top: 15px solid transparent;
+  border-right: 25px solid #8785a2;
+  margin-right: 5px;
+  cursor: pointer;
+`;
+
+const RightBtn = styled.div`
+  width: 0;
+  height: 0;
+  border-bottom: 15px solid transparent;
+  border-top: 15px solid transparent;
+  border-left: 25px solid #8785a2;
+  margin-left: 5px;
+  cursor: pointer;
+`;
+
+const ItemDiv = styled.div`
+  width: 1320px;
+  background-color: #ffe2e2;
+  margin: auto;
+  margin-top: 0;
+  margin-bottom: 0;
+  overflow: hidden;
+  @media (max-width: 1320px) {
+    width: 990px;
+  }
+  @media (max-width: 990px) {
+    width: 660px;
+  }
+  @media (max-width: 660px) {
+    width: 330px;
+  }
+
+  div {
+    display: flex;
+  }
+`;
 
 export default AdList;
