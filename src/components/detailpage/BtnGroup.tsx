20bae52c7d8845a5dcbdb4f560984a4a4bf18adb
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2023-11-07 00:59:50 +0900

    🚧 : 상세 페이지 버튼 기능 구현중

--- src/components/detailpage/BtnGroup.tsx
+++ src/components/detailpage/BtnGroup.tsx
@@ -1,12 +1,65 @@
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import PaymentModal from './PaymentModal';
+import DeleteModal from './DeleteModal';
 
 const BtnGroup = () => {
+  const [showPaymentModal, setShowPaymentModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [isBtnActive, setBtnActive] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const { postId } = useParams();
+  const navigate = useNavigate();
+
+  // 결제 모달
+  const handlePayment = () => {
+    setShowPaymentModal(!showPaymentModal);
+    setBtnActive(!isBtnActive);
+  };
+
+  // 수정 페이지로 이동
+  const handleEdit = () => {
+    navigate(`/edit/${postId}`);
+  };
+
+  // 삭제 모달
+  const handleDelete = () => {
+    setShowDeleteModal(!showDeleteModal);
+  };
+
+  // 삭제 모달 삭제/확인 버튼 선택 시
+  useEffect(() => {
+    if (isDelete) {
+      // 삭제 api
+      navigate('/');
+    } else {
+      // check 필요
+      console.log('삭제');
+      setShowDeleteModal(false);
+    }
+  }, [isDelete]);
+
+  // 모집 완료
+  const handleRecruit = () => {
+    // 모집 완료 api
+    return;
+  };
+
   return (
     <Wrapper>
-      <Btn>수정</Btn>
-      <Btn>삭제</Btn>
-      <Btn>홍보</Btn>
-      <Btn>모집 완료</Btn>
+      <Btn onClick={handleEdit}>수정</Btn>
+      <BtnWrapper>
+        <Btn onClick={handleDelete}>삭제</Btn>
+        {showDeleteModal ? <DeleteModal setIsDelete={setIsDelete} /> : ''}
+      </BtnWrapper>
+      <BtnWrapper>
+        <Btn onClick={handlePayment} className={isBtnActive ? 'isActive' : ''}>
+          홍보
+        </Btn>
+        {showPaymentModal ? <PaymentModal /> : ''}
+      </BtnWrapper>
+      <Btn onClick={handleRecruit}>모집 완료</Btn>
     </Wrapper>
   );
 };
@@ -15,13 +68,15 @@ export default BtnGroup;
 
 const Wrapper = styled.div`
   display: flex;
+  position: absolute;
+  right: 0;
 `;
 
 const Btn = styled.div`
   width: 100px;
-  height: 35px;
-  border: 1px #d9d9d9 solid;
-  border-radius: 25px;
+  height: 38px;
+  border: 2px solid var(--navy);
+  border-radius: 20px;
 
   display: flex;
   justify-content: center;
@@ -31,11 +86,21 @@ const Btn = styled.div`
   background-color: #fff;
   font-size: 16px;
   cursor: pointer;
-  transition: background-color 200ms ease-in-out color 200ms ease-in-out;
+  transition:
+    background-color 200ms ease-in-out,
+    color 200ms ease-in-out;
 
   &:hover {
     background-color: var(--navy);
     color: white;
-    border-color: white;
   }
+
+  &.isActive {
+    background-color: var(--navy);
+    color: white;
+  }
+`;
+
+const BtnWrapper = styled.div`
+  position: relative;
 `;
