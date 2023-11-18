30f88110a6846bc44a00b7eb3385555e63b00544
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2023-11-18 18:16:53 +0900

    ✨ : 선택한 태그 이미지가 화면에 나타나도록 구현

--- src/components/createPage/Dropdown.tsx
+++ src/components/createPage/Dropdown.tsx
@@ -1,27 +1,35 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import OptionMenu from './OptionMenu';
+import { categories, meeting, contact } from 'components/_common/tags';
 import { CategoryAtom, MeetingAtom, ContactAtom } from '../../recoil/Tags';
 
 interface DropdownProps {
-  type?: string;
+  type: string;
   selectedTag?: string;
   setCategory?: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Dropdown = (props: DropdownProps) => {
-  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
-  const [isContactOpen, setIsContactOpen] = useState(false);
-  const [isMeetingOpen, setIsMeetingOpen] = useState(false);
+interface TagObject {
+  [key: string]: string;
+}
 
-  const categoryInfo = useRecoilValue(CategoryAtom);
-  const meetingInfo = useRecoilValue(MeetingAtom);
-  const contactInfo = useRecoilValue(ContactAtom);
+const Dropdown = (props: DropdownProps) => {
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false); // 카테고리 선택창
+  const [isContactOpen, setIsContactOpen] = useState(false); // 지원 방법 선택창
+  const [isMeetingOpen, setIsMeetingOpen] = useState(false); // 대면/비대면 선택창
+
+  const categoryInfo = useRecoilValue(CategoryAtom); // 카테고리 태그 정보 가져오기
+  const meetingInfo = useRecoilValue(MeetingAtom); // 지원 방법 태그 정보 가져오기
+  const contactInfo = useRecoilValue(ContactAtom); // 대면/비대면 태그 정보 가져오기
 
   const handleDropdownClick = (type: string) => {
     if (type === 'category') {
+      setIsContactOpen(false);
+      setIsMeetingOpen(false);
       setIsCategoryOpen(!isCategoryOpen);
     } else if (type === 'contact') {
       setIsContactOpen(!isContactOpen);
@@ -30,11 +38,57 @@ const Dropdown = (props: DropdownProps) => {
     }
   };
 
+  // 선택된 태그 이미지 화면에 나타내기
+  const getSelectedTagImg = () => {
+    // 태그 선택 후 형식
+    const getImageInfo = (object: TagObject, selectedTag: string) => {
+      return (
+        <SelectedTagWrapper>
+          <i className="fas fa-chevron-down arrow-down" />
+          <SelectedTag src={object[selectedTag]} />
+        </SelectedTagWrapper>
+      );
+    };
+
+    // 태그 선택 전 기본 형식
+    const getDefault = () => {
+      return (
+        <span>
+          <i className="fas fa-chevron-down arrow-down" />
+          <span>선택</span>
+        </span>
+      );
+    };
+
+    let content;
+
+    switch (props.type) {
+      case 'category':
+        content = categoryInfo.isSelected
+          ? getImageInfo(categories, categoryInfo.selectedTag)
+          : getDefault();
+        break;
+      case 'contact':
+        content = contactInfo.isSelected
+          ? getImageInfo(contact, contactInfo.selectedTag)
+          : getDefault();
+        break;
+      case 'meeting':
+        content = meetingInfo.isSelected
+          ? getImageInfo(meeting, meetingInfo.selectedTag)
+          : getDefault();
+        break;
+      default:
+        content = '';
+    }
+    return content;
+  };
+
   return (
     <DropdownWrapper>
       <DropdownSelect
         type="button"
-        onClick={() => handleDropdownClick(props.type as string)}
+        onClick={() => handleDropdownClick(props.type)}
         className={
           props.type === 'category'
             ? isCategoryOpen
@@ -49,8 +103,7 @@ const Dropdown = (props: DropdownProps) => {
             : ''
         }
       >
-        <i className="fas fa-chevron-down arrow-down" />
-        선택
+        {getSelectedTagImg()}
       </DropdownSelect>
       {props.type === 'category' && isCategoryOpen && (
         <OptionMenu type={props.type} />
@@ -94,21 +147,23 @@ const DropdownSelect = styled.button`
     margin-right: 7px;
     color: #948e8e;
   }
+
+  .arrow-up {
+    transform: rotate(180deg);
+  }
 `;
 
-// const SelectedTagWrapper = styled.div`
-//   width: 100px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   /* overflow: hidden; */
-//   margin-right: 5px;
-//   transition: opacity 100ms ease-in-out;
-//   cursor: pointer;
-// `;
+const SelectedTagWrapper = styled.div`
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: opacity 100ms ease-in-out;
+  cursor: pointer;
+`;
 
-// const SelectedTag = styled.img`
-//   width: 100%;
-//   height: 100%;
-//   object-fit: cover;
-// `;
+const SelectedTag = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
