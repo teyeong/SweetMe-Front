ec40f5047fdde6496bc45b0eac7ebd56cd6dec05
teyeong <taetae0202@naver.com>
2023-11-24 11:19:48 +0900

    ✨ : 로그인 유무 확인

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,14 +1,32 @@
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { LoginStateAtom } from 'recoil/Login';
+import { useEffect } from 'react';
 
 import Header from 'components/_common/Header';
 import Form from 'components/createpage/Form';
 import { Container } from 'components/_common/pageLayout';
 
 const CreatePage = () => {
+  const isLogin = useRecoilValue(LoginStateAtom);
+
+  useEffect(() => {
+    if (!isLogin) {
+      alert('로그인이 필요합니다.');
+      window.location.href = '/';
+    }
+  }, []);
+
   return (
     <Container>
-      <Header />
-      <Form />
+      {isLogin ? (
+        <>
+          <Header />
+          <Form />
+        </>
+      ) : (
+        <></>
+      )}
     </Container>
   );
 };
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,11 +1,30 @@
 import Header from 'components/_common/Header';
 import MenuBar from 'components/mypage/MeneBar';
+import { LoginStateAtom } from 'recoil/Login';
+
+import { useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
 
 const MyPage = () => {
+  const loginState = useRecoilValue(LoginStateAtom);
+
+  useEffect(() => {
+    if (!loginState) {
+      alert('로그인이 필요합니다.');
+      window.location.href = '/';
+    }
+  }, []);
+
   return (
     <>
-      <Header />
-      <MenuBar />
+      {loginState ? (
+        <>
+          <Header />
+          <MenuBar />
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
